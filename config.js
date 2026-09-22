6585e54f3f4a1554db1b03ae06ab6333ad34c99c
lamaritaspentig <smpn03mksr@gmail.com>
2026-09-22 15:37:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CF_DNS_CONFIG = {
   // Tempel URL deployment Google Apps Script yang berakhir dengan /exec
-  APPS_SCRIPT_URL: ''
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbypV77m2uX4bMliO8z_B7yVt0aFkGMaUDkRc3-HccYhv-T0xWlyeZjY4lkZ7ITeseGv/exec'
 };
